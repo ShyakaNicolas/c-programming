82c3bb4d6240c254d7085afb44f4114fe8e7dd5f
Shyaka Nicolas <shyakaking12@gmail.com>
2021-09-13 19:57:47 +0200

    my cprogram is added

---
+++ home work 1.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+main ()
+{/*int mydays;
+int years:
+int weeks;
+int days;
+printf("enter days you have:");
+scanf("%d",&mydays);
+years=mydays%*/
+int r;
+int d=r*r;
+int pie=3.14;
+int c=d*pie;
+int area=;
+printf("enter the radius;");
+scanf("%d",&r);
+printf("\nthe diameter is:%d",r*r);
+printf("\nthe circumference of this circle is:%d",d*pie);
+printf("the area of this circle is:%d",);
+
+
+int marks1;
+int marks2;
+int marks3;
+int marks4;
+int marks5;
+int total;
+int average;
+
+printf("enter the marks of subject1:");
+scanf("%d",marks1);
+printf("enter the marks of subject2:");
+scanf("%d",marks1);
+printf("enter the marks of subject3:");
+scanf("%d",marks1);
+printf("enter the marks of subject4:");
+scanf("%d",marks1);
+printf("enter the marks of subject5:");
+scanf("%d",marks1);
+total=marks1+marks2+marks3+marks4+marks5;
+average=total/5
+
+printf("the total marks is:",total);
+printf("the average marks is:",average);
+
+
+
+
+
+
+
